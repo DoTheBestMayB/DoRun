4310e697ae1723f391bd01153a31945ace23f69c
dothebestmayb <send1684@gmail.com>
2024-12-24 06:05:31 +0900

    build: Kotlin 2.1, targetSDK 35로 변경 및 호환성 수정

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,6 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
-    alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.jetbrains.kotlin.jvm) apply false
     alias(libs.plugins.ksp) apply false
@@ -10,4 +9,5 @@ plugins {
     alias(libs.plugins.room) apply false
     alias(libs.plugins.androidDynamicFeature) apply false
     alias(libs.plugins.mapsplatform.secrets.plugin) apply false
+    alias(libs.plugins.compose.compiler) apply false
 }
