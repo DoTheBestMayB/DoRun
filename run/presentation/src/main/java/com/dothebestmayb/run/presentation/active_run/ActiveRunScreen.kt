1f65137eb6393aa23c15aa1d06a3be89ddf9f491
DoTheBestMayB <send1684@gmail.com>
2025-01-14 10:42:47 +0900

    feat: 권한 요청 처리하기

--- run/presentation/src/main/java/com/dothebestmayb/run/presentation/active_run/ActiveRunScreen.kt
+++ run/presentation/src/main/java/com/dothebestmayb/run/presentation/active_run/ActiveRunScreen.kt
@@ -2,6 +2,13 @@
 
 package com.dothebestmayb.run.presentation.active_run
 
+import android.Manifest
+import android.content.Context
+import android.os.Build
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,18 +17,26 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.dothebestmayb.core.presentation.designsystem.DoRunTheme
 import com.dothebestmayb.core.presentation.designsystem.StartIcon
 import com.dothebestmayb.core.presentation.designsystem.StopIcon
+import com.dothebestmayb.core.presentation.designsystem.components.DoRunDialog
 import com.dothebestmayb.core.presentation.designsystem.components.DoRunFloatingActionButton
+import com.dothebestmayb.core.presentation.designsystem.components.DoRunOutlinedActionButton
 import com.dothebestmayb.core.presentation.designsystem.components.DoRunScaffold
 import com.dothebestmayb.core.presentation.designsystem.components.DoRunToolbar
 import com.dothebestmayb.run.presentation.R
 import com.dothebestmayb.run.presentation.active_run.components.RunDataCard
+import com.dothebestmayb.run.presentation.util.hasLocationPermission
+import com.dothebestmayb.run.presentation.util.hasNotificationPermission
+import com.dothebestmayb.run.presentation.util.shouldShowLocationPermissionRationale
+import com.dothebestmayb.run.presentation.util.shouldShowNotificationPermissionRationale
 import org.koin.androidx.compose.koinViewModel
 
 @Composable
@@ -39,6 +54,61 @@ private fun ActiveRunEventScreen(
     state: ActiveRunState,
     onAction: (ActiveRunAction) -> Unit
 ) {
+    val context = LocalContext.current
+
+    val permissionLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.RequestMultiplePermissions()
+    ) { perms ->
+        val hasCourseLocationPermission = perms[Manifest.permission.ACCESS_COARSE_LOCATION] == true
+        val hasFineLocationPermission = perms[Manifest.permission.ACCESS_FINE_LOCATION] == true
+        val hasNotificationPermission = if (Build.VERSION.SDK_INT >= 33) {
+            perms[Manifest.permission.POST_NOTIFICATIONS] == true
+        } else {
+            true
+        }
+
+        val activity = context as ComponentActivity
+        val showLocationRationale = activity.shouldShowLocationPermissionRationale()
+        val showNotificationRationale = activity.shouldShowNotificationPermissionRationale()
+
+        onAction(
+            ActiveRunAction.SubmitLocationPermissionInfo(
+                acceptedLocationPermission = hasCourseLocationPermission && hasFineLocationPermission,
+                showLocationRationale = showLocationRationale,
+            )
+        )
+        onAction(
+            ActiveRunAction.SubmitNotificationPermissionInfo(
+                acceptedNotificationPermission = hasNotificationPermission,
+                showNotificationPermissionRationale = showNotificationRationale,
+            )
+        )
+    }
+
+    // 화면이 처음 보여질 때, 권한 처리
+    LaunchedEffect(key1 = true) {
+        val activity = context as ComponentActivity
+        val showLocationRationale = activity.shouldShowLocationPermissionRationale()
+        val showNotificationRationale = activity.shouldShowNotificationPermissionRationale()
+
+        onAction(
+            ActiveRunAction.SubmitLocationPermissionInfo(
+                acceptedLocationPermission = context.hasLocationPermission(),
+                showLocationRationale = showLocationRationale,
+            )
+        )
+        onAction(
+            ActiveRunAction.SubmitNotificationPermissionInfo(
+                acceptedNotificationPermission = context.hasNotificationPermission(),
+                showNotificationPermissionRationale = showNotificationRationale,
+            )
+        )
+
+        if (!showLocationRationale && !showNotificationRationale) {
+            permissionLauncher.requestDoRunPermissions(context)
+        }
+    }
+
     DoRunScaffold(
         withGradient = false,
         topAppBar = {
@@ -83,6 +153,59 @@ private fun ActiveRunEventScreen(
                     .fillMaxWidth()
             )
         }
+    }
+
+    if (state.showLocationRationale || state.showNotificationRationale) {
+        DoRunDialog(
+            title = stringResource(R.string.permission_required),
+            onDismiss = { /* 실수로 권한 창을 닫지 않도록 하기 위해 허용하지 않음 */ },
+            description = when {
+                state.showLocationRationale && state.showNotificationRationale -> {
+                    stringResource(id = R.string.location_notification_rationale)
+                }
+                state.showLocationRationale -> {
+                    stringResource(id = R.string.location_rationale)
+                }
+                else -> {
+                    stringResource(id = R.string.notification_rationale)
+                }
+            },
+            primaryButton = {
+                DoRunOutlinedActionButton(
+                    text = stringResource(id = R.string.okay),
+                    isLoading = false,
+                    onClick = {
+                        onAction(ActiveRunAction.DismissRationaleDialog)
+                        permissionLauncher.requestDoRunPermissions(context)
+                    }
+                )
+            },
+        )
+    }
+}
+
+private fun ActivityResultLauncher<Array<String>>.requestDoRunPermissions(
+    context: Context
+) {
+    val hasLocationPermission = context.hasLocationPermission()
+    val hasNotificationPermission = context.hasNotificationPermission()
+
+    val locationPermissions = arrayOf(
+        Manifest.permission.ACCESS_COARSE_LOCATION,
+        Manifest.permission.ACCESS_FINE_LOCATION,
+    )
+    val notificationPermission = if (Build.VERSION.SDK_INT >= 33) {
+        arrayOf(Manifest.permission.POST_NOTIFICATIONS)
+    } else {
+        arrayOf()
+    }
+
+    when {
+        !hasLocationPermission && !hasNotificationPermission -> {
+            launch(locationPermissions + notificationPermission)
+        }
+        !hasLocationPermission -> launch(locationPermissions)
+        !hasNotificationPermission -> launch(notificationPermission)
     }
 }
 
